b174c015c8daf661e39af87c44b621592883fde3
Nadav Zilberman <nadavzil7@gmail.com>
2024-10-24 15:40:46 +0300

    fixes

--- signal_to_noise_sol.py
+++ signal_to_noise_sol.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 from scipy.ndimage import gaussian_filter
 
 DECIMATION_RATE = 4
-SLICE_DURATION = 1000
-NFFT = 1024  # for the first 30 seconds, 1024 was enough, for after I needed to take it up to 8192 beause the data was spread over a smaller freq band.
+NFFT = 1024 
+# NFFT = 8192
 
 # Read data
 filename = 'signal_to_noise.sigmf-data'
@@ -16,6 +16,7 @@ sample_rate = signal.get_global_field(SigMFFile.SAMPLE_RATE_KEY)
 sample_count = signal.sample_count
 signal_duration = sample_count / sample_rate
 
+# decimate less interesting parts
 decimated_signal = decimate(samples, DECIMATION_RATE, ftype='fir')
 plt.figure("spectogram")
 all_samples_specgram, orig_f, orig_t, _ = plt.specgram(decimated_signal, NFFT=NFFT, Fs=sample_rate/DECIMATION_RATE, mode="magnitude")
